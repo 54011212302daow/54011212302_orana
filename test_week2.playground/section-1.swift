f06157553a858118c97f57c6ad077546835d38d4
54011212302daow <orana2535@gmail.com>
2015-01-23 14:54:32 +0700

    week_2

---
+++ test_week2.playground/section-1.swift
@@ -0,0 +1,44 @@
+import UIKit
+
+var str = "Hello, playground"
+
+
+class TipCalculator {
+    
+    //2
+    let total: Double
+    let taxPct: Double
+    let subtotal: Double
+    
+    //3
+    init(total:Double, taxPct:Double){
+        self.total = total
+        self.taxPct = taxPct
+        subtotal = total / (taxPct + 1)
+    }
+    
+    //4
+    func calcTipWithTipPct(tipPct:Double) ->Double{
+        return subtotal * tipPct
+    }
+    
+    //1
+    func returnPossibleTips()->[Int: Double]{
+        let possibleTipsInferred = [0.15, 0.18, 0.20]
+        let possibleTipsExplicit:[Double] = [0.15, 0.18, 0.20]
+        var numberofItems = possibleTipsInferred.count
+        
+        //2
+        var retval = [Int: Double]()
+        for possibleTip in possibleTipsInferred{
+            let intPct = Int(possibleTip*100)
+            //3
+            retval[intPct] = calcTipWithTipPct(possibleTip)
+        }
+        return retval
+    }
+    
+}
+//
+let tipCalc = TipCalculator(total: 33.25, taxPct: 0.06)
+tipCalc.returnPossibleTips()
