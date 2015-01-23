065baa30f375742951721b9eafe352b9e2fee963
54011212302daow <orana2535@gmail.com>
2015-01-23 16:19:26 +0700

    Dictionary

---
+++ test2_week2.playground/section-1.swift
@@ -0,0 +1,26 @@
+// Playground - noun: a place where people can play
+
+/*import UIKit
+
+var str = "Hello, playground"*/
+
+var shoppingList: [String] = ["Eggs","Milk"]
+let possibleTipsInferred  = [0.15,0.18,0.20]
+let possibleTipsExplicit:[Double] = [0.15,0.18,0.20]
+var number0fItems = possibleTipsInferred.count
+
+shoppingList.isEmpty
+
+//var firstItem = shoppingList[3]
+
+shoppingList += ["dog","fish"]
+shoppingList.insert("Maple Syrup", atIndex: 0)
+shoppingList.append("Flour")
+shoppingList += ["Baking Powder"]
+
+var firstItem = shoppingList[0]
+
+shoppingList[4...6] = ["Bananas","Apples"]
+
+let apples = shoppingList.removeLast()
+let mapleSyrup = shoppingList.removeAtIndex(0)
